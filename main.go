f4afd599f4baa2d248f510576293fcd0268f4462
JED <jandedjedi72@gmail.com>
2025-01-30 12:23:06 +0100

    updated main.go
    main.go is leaner and more efficient

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type Response struct {
+	Email           string `json:"email"`
+	CurrentDatetime string `json:"current_datetime"`
+	GithubURL       string `json:"github_url"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// setting CORS headers for CORS handling
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	// preparing response
+	response := Response{
+		Email:           "jedotoekong@gmail.com",                 // hng registered email
+		CurrentDatetime: time.Now().UTC().Format(time.RFC3339),   //  current datetime in ISO 8601 format (yymmdd h-m-s-ms)
+		GithubURL:       "https://github.com/Jedway/jed-bga.git", // my github profile
+	}
+
+	// encode the response in JSON format
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
